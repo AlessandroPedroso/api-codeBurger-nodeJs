e91e739b7bb41ab7b5bfbf251c87ec5e13a656f3
AlessandroPedroso <alessandropedrosoti@gmail.com>
2023-03-06 19:14:30 -0300

    alteração realizado na tabela catogorias, adicionado o campo path alteração do file pelo metodo de update

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -22,6 +22,7 @@ class CategoryController {
     }
 
     const { name } = request.body
+    const { filename: path } = request.file
 
     const categoryExists = await Category.findOne({
       where: { name },
@@ -32,7 +33,7 @@ class CategoryController {
     }
 
     // const category = await Category.create({ name })
-    const { id } = await Category.create({ name })
+    const { id } = await Category.create({ name, path })
 
     // const newCategory = {
     //   id: category.id,
@@ -46,6 +47,62 @@ class CategoryController {
     const category = await Category.findAll()
 
     return response.json(category)
+  }
+
+  async update(request, response) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+    })
+
+    try {
+      await schema.validateSync(request.body, { abortEarly: false })
+    } catch (err) {
+      // console.log(err)
+      return response.status(400).json({ error: err.errors })
+    }
+
+    const { admin: isAdmin } = await User.findByPk(request.userId)
+
+    if (!isAdmin) {
+      return response.status(401).json()
+    }
+
+    const { name } = request.body
+
+    const { id } = request.params
+
+    const category = await Category.findByPk(id)
+
+    if (!category) {
+      return response
+        .status(401)
+        .json({ error: 'Make sure your category id is correct' })
+    }
+
+    let path
+    if (request.file) {
+      path = request.file.filename
+    }
+
+    // const { filename: path } = request.file
+
+    const categoryExists = await Category.findOne({
+      where: { name },
+    })
+
+    if (categoryExists) {
+      return response.status(400).json({ error: 'Category already exists' })
+    }
+
+    // const category = await Category.create({ name })
+    await Category.update({ name, path }, { where: { id } })
+
+    // const newCategory = {
+    //   id: category.id,
+    //   name: category.name,
+    // }
+
+    return response.status(200).json()
   }
 }
 
