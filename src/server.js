3167ff7cf4baac8239b589b7ef9081078b8cd5b1
Alessandro Pedroso <alessandropedrosoti@gmail.com>
2023-06-29 00:43:17 -0300

    atualizando o host para funcionar no railway

--- src/server.js
+++ src/server.js
@@ -1,5 +1,11 @@
 // //const app = require('./app')
 import app from './app'
-const port = 3001
+// const port = 3001
 
-app.listen(port)
+// app.listen(port)
+
+// precisa adicionar dessa maneira para funcionar no railway
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, "0.0.0.0")
